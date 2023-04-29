2c242a7fe088c74ef8e6cc31c14e3e0e10c1586b
Rothana69 <godkiller69@gmail.com>
2023-04-29 16:14:56 +0700

    [#14] get flight of pilot

--- src/company/Airline.ts
+++ src/company/Airline.ts
@@ -1,13 +1,27 @@
+import { Date } from "../DateTime/Date";
 import { Booking } from "../booking/Booking";
 import { Flight } from "../flight/Flight";
+import { Employee } from "../human/staff/Employee";
+import { Pilot } from "../human/staff/pilot/Pilot";
 
-export class Aeroplane{
+export class Airline {
     private airlineName: string;
-    private flights: Flight[];
-    private aeroplane : Aeroplane[];
-    private booking : Booking[];
-    constructor(airlineName: string){
+    private flights: Flight[]=[];
+    private booking: Booking[];
+    constructor(airlineName: string) {
         this.airlineName = airlineName;
     }
-    
+    addflight(flight: Flight) {
+        this.flights.push(flight);
+    }
+    getFlights(pilot: Pilot, date: Date): Flight[] {
+        let flightOfPilot: Flight[] = []
+        this.flights.forEach(flight => {
+            if (flight.getPilot() == pilot && flight.getDate() == date) {
+                flightOfPilot.push(flight);
+            }
+        })
+        return flightOfPilot;
+    }
+
 }
